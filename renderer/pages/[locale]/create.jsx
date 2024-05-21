a6010d3988a5544e7a3b9b9aac87319c86b1af5e
Pavel <braurs64@gmail.com>
2024-05-21 15:07:56 +0400

    fix: upd translate

--- renderer/pages/[locale]/create.jsx
+++ renderer/pages/[locale]/create.jsx
@@ -1,55 +1,55 @@
-import React from "react";
-import Head from "next/head";
-import Link from "next/link";
-import { useTranslation } from "react-i18next";
-import { makeStaticProperties, getStaticPaths } from "../../lib/get-static";
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import { useTranslation } from 'react-i18next'
+import { makeStaticProperties, getStaticPaths } from '../../lib/get-static'
 
 export default function Create() {
   const {
     i18n: { language: locale },
     t,
-  } = useTranslation(["common", "projects"]);
-  const [fileUrl, setFileUrl] = React.useState(false);
+  } = useTranslation(['common', 'projects'])
+  const [fileUrl, setFileUrl] = React.useState(false)
   const onSubmit = (e) => {
-    e.preventDefault();
-    window.electronAPI.addProject(fileUrl);
-  };
+    e.preventDefault()
+    window.electronAPI.addProject(fileUrl)
+  }
 
   return (
     <>
       <Head>
-        <title>{t("V-CANA")}</title>
+        <title>{t('V-CANA')}</title>
       </Head>
       <div className="text-2xl w-full">
         <Link href={`/${locale}/account`} legacyBehavior>
-          <a className="btn-primary text-base">{t("Back")}</a>
+          <a className="btn-primary text-base">{t('Back')}</a>
         </Link>
         <br />
-        <h2>{t("projects:ImportProject")}</h2>
+        <h2>{t('projects:ImportProject')}</h2>
         <form onSubmit={onSubmit}>
           <button
             className="btn-primary text-base mt-3"
             onClick={async (e) => {
-              e.preventDefault();
-              const filePath = await window.electronAPI.openFile();
-              setFileUrl(filePath);
+              e.preventDefault()
+              const filePath = await window.electronAPI.openFile()
+              setFileUrl(filePath)
             }}
           >
-            {t("projects:SelectArchiveProject")}
+            {t('projects:SelectArchiveProject')}
           </button>
-          <p>{fileUrl || t("NotSelected")}</p>
+          <p>{fileUrl || t('NotSelected')}</p>
 
           <input
             className="btn-primary text-base mt-3"
             type="submit"
-            value={t("Create")}
+            value={t('Import')}
           />
         </form>
       </div>
     </>
-  );
+  )
 }
 
-export const getStaticProps = makeStaticProperties(["common", "projects"]);
+export const getStaticProps = makeStaticProperties(['common', 'projects'])
 
-export { getStaticPaths };
+export { getStaticPaths }
