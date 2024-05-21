023e47d7219afabbe3f0010a40dc90d2c7179356
Pavel <braurs64@gmail.com>
2024-05-21 15:02:22 +0400

    fix: upd ProjectsList

--- renderer/components/ProjectsList.jsx
+++ renderer/components/ProjectsList.jsx
@@ -1,57 +1,59 @@
-import { useState } from "react";
+import { useState } from 'react'
 
-import Link from "next/link";
-import { useRouter } from "next/router";
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-import { useTranslation } from "react-i18next";
-import jszip from "jszip";
+import { useTranslation } from 'react-i18next'
+import jszip from 'jszip'
 
-import { JsonToPdf } from "@texttree/obs-format-convert-rcl";
+import { JsonToPdf } from '@texttree/obs-format-convert-rcl'
 
-import ListBox from "./ListBox";
-import Modal from "./Modal";
-import ChaptersMerger from "./ChaptersMerger";
+import ListBox from './ListBox'
+import Modal from './Modal'
+import ChaptersMerger from './ChaptersMerger'
+
+import DownloadIcon from '../public/icons/download.svg'
 
 const styles = {
   currentPage: {
     fontSize: 16,
-    alignment: "center",
+    alignment: 'center',
     bold: true,
     margin: [0, 10, 0, 0],
   },
   chapterTitle: { fontSize: 20, bold: true, margin: [0, 26, 0, 15] },
   verseNumber: { sup: true, bold: true, opacity: 0.8, margin: [0, 8, 8, 0] },
-  defaultPageHeader: { bold: true, width: "50%" },
-  text: { alignment: "justify" },
-};
+  defaultPageHeader: { bold: true, width: '50%' },
+  text: { alignment: 'justify' },
+}
 
 function ProjectsList({ projects }) {
   const {
     i18n: { language: locale },
     t,
-  } = useTranslation(["common", "projects"]);
+  } = useTranslation(['common', 'projects'])
   const options = [
-    { label: t("projects:ExportToPDF"), value: "pdf" },
-    { label: t("projects:ExportToZIP"), value: "zip" },
-    { label: t("projects:ExportToUSFM"), value: "usfm" },
-  ];
-  const { pathname } = useRouter();
-  const [selectedOption, setSelectedOption] = useState(options[0].value);
-  const [currentProject, setCurrentProject] = useState(null);
-  const [isOpenModal, setIsOpenModal] = useState(false);
+    { label: t('projects:ExportToPDF'), value: 'pdf' },
+    { label: t('projects:ExportToZIP'), value: 'zip' },
+    { label: t('projects:ExportToUSFM'), value: 'usfm' },
+  ]
+  const { pathname } = useRouter()
+  const [selectedOption, setSelectedOption] = useState(options[0].value)
+  const [currentProject, setCurrentProject] = useState(null)
+  const [isOpenModal, setIsOpenModal] = useState(false)
   const exportToPdf = (chapters) => {
-    const book = [];
+    const book = []
     for (const chapterNum in chapters) {
       if (Object.hasOwnProperty.call(chapters, chapterNum)) {
         const chapter = Object.entries(chapters[chapterNum]).map(([k, v]) => ({
           verse: k,
           text: v.text,
           enabled: v.enabled,
-        }));
+        }))
         book.push({
-          title: "Chapter " + chapterNum,
+          title: 'Chapter ' + chapterNum,
           verseObjects: chapter,
-        });
+        })
       }
     }
 
@@ -64,74 +66,65 @@ function ProjectsList({ projects }) {
       showPageFooters: false,
       styles,
     })
-      .then(() => console.log("PDF creation completed"))
-      .catch((error) => console.error("PDF creation failed:", error));
-  };
+      .then(() => console.log('PDF creation completed'))
+      .catch((error) => console.error('PDF creation failed:', error))
+  }
   const exportToZip = (chapters) => {
     try {
       if (!chapters) {
-        throw new Error(t("NoData"));
+        throw new Error(t('NoData'))
       }
-      const jsonContent = JSON.stringify(chapters, null, 2);
-      const zip = new jszip();
-      const currentDate = new Date();
-      const formattedDate = currentDate.toISOString().split("T")[0];
-      const fileName = `chapters_${formattedDate}.json`;
-      zip.file(fileName, jsonContent);
-      zip.generateAsync({ type: "blob" }).then(function (content) {
-        const blob = content;
-        const url = window.URL.createObjectURL(blob);
-        const downloadLink = document.createElement("a");
-        downloadLink.href = url;
-        downloadLink.download = `chapters_${formattedDate}.zip`;
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeChild(downloadLink);
-        window.URL.revokeObjectURL(url);
-      });
+      const jsonContent = JSON.stringify(chapters, null, 2)
+      const zip = new jszip()
+      const currentDate = new Date()
+      const formattedDate = currentDate.toISOString().split('T')[0]
+      const fileName = `chapters_${formattedDate}.json`
+      zip.file(fileName, jsonContent)
+      zip.generateAsync({ type: 'blob' }).then(function (content) {
+        const blob = content
+        const url = window.URL.createObjectURL(blob)
+        const downloadLink = document.createElement('a')
+        downloadLink.href = url
+        downloadLink.download = `chapters_${formattedDate}.zip`
+        document.body.appendChild(downloadLink)
+        downloadLink.click()
+        document.body.removeChild(downloadLink)
+        window.URL.revokeObjectURL(url)
+      })
     } catch (error) {
-      console.log(error.message);
+      console.log(error.message)
     }
-  };
+  }
   const download = (project) => {
-    const chapters = window.electronAPI.getBook(project.id);
-    if (selectedOption === "pdf") {
-      exportToPdf(chapters);
+    const chapters = window.electronAPI.getBook(project.id)
+    if (selectedOption === 'pdf') {
+      exportToPdf(chapters)
     } else {
-      exportToZip(chapters);
+      exportToZip(chapters)
     }
-  };
+  }
   return (
     <>
       <table className="border-collapse table-auto w-full text-sm">
         <thead>
-          <tr>
-            <th className="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">
-              {t("Book")}
-            </th>
-            <th className="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">
-              {t("projects:Project")}
-            </th>
-            <th className="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">
-              {t("projects:Method")}
-            </th>
-            <th className="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">
-              {t("ID")}
-            </th>
-            <th className="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">
-              {t("CreatedAt")}
-            </th>
-            <th className="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left"></th>
+          <tr className="text-left text-th-secondary-300 border-b border-th-secondary-200 cursor-default">
+            <th className="font-medium pt-0 pr-4 pb-3 pl-8">{t('Book')}</th>
+            <th className="font-medium pt-0 pr-4 pb-3 pl-8">{t('projects:Project')}</th>
+            <th className="font-medium pt-0 pr-4 pb-3 pl-8">{t('projects:Method')}</th>
+            <th className="font-medium pt-0 pr-4 pb-3 pl-8">{t('ID')}</th>
+            <th className="font-medium pt-0 pr-4 pb-3 pl-8">{t('CreatedAt')}</th>
+            <th className="font-medium pt-0 pr-4 pb-3 pl-8"></th>
           </tr>
         </thead>
-        <tbody className="bg-white">
+        <tbody className="bg-th-secondary-10">
           {projects.map((project) => (
-            <tr key={project.id}>
-              <td className="border-b border-slate-100 p-4 pl-8 text-slate-500">
+            <tr
+              key={project.id}
+              className="border-b border-th-secondary-200 text-th-primary-100"
+            >
+              <td className="p-4 pl-8">
                 <Link
-                  href={`${pathname.replace("[locale]", locale)}/project/${
-                    project.id
-                  }`}
+                  href={`${pathname.replace('[locale]', locale)}/project/${project.id}`}
                   legacyBehavior
                 >
                   <a className="font-bold underline">
@@ -139,27 +132,21 @@ function ProjectsList({ projects }) {
                   </a>
                 </Link>
               </td>
-              <td className="border-b border-slate-100 p-4 pl-8 text-slate-500">
-                {project.name}
-              </td>
-              <td className="border-b border-slate-100 p-4 pl-8 text-slate-500">
-                {project.method}
-              </td>
-              <td className="border-b border-slate-100 p-4 pl-8 text-slate-500">
-                {project.id}
-              </td>
-              <td className="border-b border-slate-100 p-4 pl-8 text-slate-500">
+              <td className="p-4 pl-8">{project.name}</td>
+              <td className="p-4 pl-8">{project.method}</td>
+              <td className="p-4 pl-8">{project.id}</td>
+              <td className="p-4 pl-8">
                 {new Date(project.createdAt).toLocaleDateString()}
               </td>
-              <td className="border-b border-slate-100 p-4 pl-8 text-slate-500">
-                <div
-                  className="btn-primary"
-                  onClick={() => {
-                    setCurrentProject(project);
-                    setIsOpenModal(true);
-                  }}
-                >
-                  {t("Download")}
+              <td className="p-4 pl-8">
+                <div className="flex justify-center cursor-pointer">
+                  <DownloadIcon
+                    className="w-8 hover:opacity-70"
+                    onClick={() => {
+                      setCurrentProject(project)
+                      setIsOpenModal(true)
+                    }}
+                  />
                 </div>
               </td>
             </tr>
@@ -170,7 +157,7 @@ function ProjectsList({ projects }) {
         closeHandle={() => setIsOpenModal(false)}
         className={{
           dialogPanel:
-            "w-full max-w-md align-middle p-6 bg-th-primary-100 text-th-text-secondary-100 overflow-y-visible rounded-3xl",
+            'w-full max-w-md align-middle p-6 bg-th-primary-100 text-th-text-secondary-100 overflow-y-visible rounded-3xl',
         }}
         isOpen={isOpenModal}
       >
@@ -179,30 +166,25 @@ function ProjectsList({ projects }) {
           setSelectedOption={setSelectedOption}
           options={options}
         />
-        {selectedOption === "usfm" && (
-          <ChaptersMerger book={currentProject.book.code} />
-        )}
+        {selectedOption === 'usfm' && <ChaptersMerger book={currentProject.book.code} />}
         <div className="flex justify-center">
           <div className="flex gap-4 text-xl w-1/2">
-            <button
-              className="btn-primary flex-1"
-              onClick={() => setIsOpenModal(false)}
-            >
-              {t("Close")}
+            <button className="btn-primary flex-1" onClick={() => setIsOpenModal(false)}>
+              {t('Close')}
             </button>
-            {selectedOption !== "usfm" && (
+            {selectedOption !== 'usfm' && (
               <button
                 className="btn-primary flex-1"
                 onClick={() => download(currentProject)}
               >
-                {t("Download")}
+                {t('Download')}
               </button>
             )}
           </div>
         </div>
       </Modal>
     </>
-  );
+  )
 }
 
-export default ProjectsList;
+export default ProjectsList
