b5cfdd91e5940472b156d9b77c0b1052d77cadac
Pavel <braurs64@gmail.com>
2024-05-21 19:08:05 +0400

    fix: upd fileName in handleDownloadChapter

--- renderer/components/ChaptersList.jsx
+++ renderer/components/ChaptersList.jsx
@@ -1,46 +1,49 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-import { JsonToPdf } from "@texttree/obs-format-convert-rcl";
-import { useTranslation } from "react-i18next";
+import { JsonToPdf } from '@texttree/obs-format-convert-rcl'
+import { useTranslation } from 'react-i18next'
 
 const styles = {
   currentPage: {
     fontSize: 16,
-    alignment: "center",
+    alignment: 'center',
     bold: true,
     margin: [0, 10, 0, 0],
   },
   chapterTitle: { fontSize: 20, bold: true, margin: [0, 26, 0, 15] },
   verseNumber: { sup: true, bold: true, opacity: 0.8, margin: [0, 8, 8, 0] },
-  defaultPageHeader: { bold: true, width: "50%" },
-  text: { alignment: "justify" },
-};
+  defaultPageHeader: { bold: true, width: '50%' },
+  text: { alignment: 'justify' },
+}
 
 function ChapterList({ id, chapters, steps, mutate }) {
   const {
     i18n: { language: locale },
     t,
-  } = useTranslation(["projects", "common"]);
-  const { pathname } = useRouter();
+  } = useTranslation(['projects', 'common'])
+  const { pathname } = useRouter()
 
   const handleBackStep = (chapter, step) => {
-    const backStep = window.electronAPI.goToStep(id, chapter, step - 1);
+    const backStep = window.electronAPI.goToStep(id, chapter, step - 1)
     if (backStep !== step) {
-      mutate();
+      mutate()
     }
-  };
+  }
   const handleDownloadChapter = (chapter) => {
-    const savedVerses = Object.entries(
-      window.electronAPI.getChapter(id, chapter)
-    )
+    const savedVerses = Object.entries(window.electronAPI.getChapter(id, chapter))
       .map(([k, v]) => ({ verse: k, text: v.text, enabled: v.enabled }))
-      .filter((v) => v.enabled);
-    const filename = "chapter_" + chapter;
+      .filter((v) => v.enabled)
+
+    const project = window.electronAPI.getProject(id)
+    const currentDate = new Date().toISOString().slice(0, 10)
+
+    const fileName = `${project.book.code}_chapter_${chapter}_${currentDate}`
+
     JsonToPdf({
-      data: [{ title: "Chapter " + chapter, verseObjects: savedVerses }],
+      data: [{ title: 'Chapter ' + chapter, verseObjects: savedVerses }],
       styles,
-      filename,
+      fileName,
       showImages: false,
       combineVerses: false,
       showChapterTitlePage: false,
@@ -48,24 +51,24 @@ function ChapterList({ id, chapters, steps, mutate }) {
       bookPropertiesObs: {},
       showPageFooters: false,
     })
-      .then(() => console.log("PDF creation completed"))
-      .catch((error) => console.error("PDF creation failed:", error));
-  };
+      .then(() => console.log('PDF creation completed'))
+      .catch((error) => console.error('PDF creation failed:', error))
+  }
   return (
     <table className="border-collapse table-auto w-full text-sm">
       <thead>
         <tr>
           <th className="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">
-            {t("Chapter")}
+            {t('Chapter')}
           </th>
           <th className="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">
-            {t("Step")}
+            {t('Step')}
           </th>
           <th className="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">
-            {t("StepBack")}
+            {t('StepBack')}
           </th>
           <th className="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">
-            {t("common:Download")}
+            {t('common:Download')}
           </th>
         </tr>
       </thead>
@@ -75,12 +78,12 @@ function ChapterList({ id, chapters, steps, mutate }) {
             <td className="border-b border-slate-100 p-4 pl-8 text-slate-500">
               <Link
                 href={`${pathname
-                  .replace("[locale]", locale)
-                  .replace("[id]", id)}/${chapter}/${step}`}
+                  .replace('[locale]', locale)
+                  .replace('[id]', id)}/${chapter}/${step}`}
                 legacyBehavior
               >
                 <a className="font-bold underline">
-                  {t("Chapter")} {chapter}
+                  {t('Chapter')} {chapter}
                 </a>
               </Link>
             </td>
@@ -93,7 +96,7 @@ function ChapterList({ id, chapters, steps, mutate }) {
                   className="btn-primary text-base"
                   onClick={() => handleBackStep(chapter, step)}
                 >
-                  {t("GoToStep")} {step}
+                  {t('GoToStep')} {step}
                 </div>
               )}
             </td>
@@ -102,14 +105,14 @@ function ChapterList({ id, chapters, steps, mutate }) {
                 className="btn-primary text-base"
                 onClick={() => handleDownloadChapter(chapter)}
               >
-                {t("common:Download")}
+                {t('common:Download')}
               </div>
             </td>
           </tr>
         ))}
       </tbody>
     </table>
-  );
+  )
 }
 
-export default ChapterList;
+export default ChapterList
