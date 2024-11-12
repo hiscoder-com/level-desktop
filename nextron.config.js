4bef5e2ba54be27a156adb7bfad1b5379d313079
DenisArger <den.arger@gmail.com>
2024-11-12 13:50:46 +0300

    feat:

--- nextron.config.js
+++ nextron.config.js
@@ -1,5 +1,13 @@
+const webpack = require('webpack')
+require('dotenv').config({ path: './renderer/.env' })
 module.exports = {
   webpack: (defaultConfig) => {
+    defaultConfig.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env': JSON.stringify(process.env),
+      })
+    )
+
     return Object.assign(defaultConfig, {
       entry: {
         preload: './main/preload.js',
