6ed7f29cfc12865d73b58d0ecb79dc082d60df06
DenisArger <den.arger@gmail.com>
2025-02-11 16:11:07 +0300

    fix: mrror change

--- renderer/hooks/useRtlInput.js
+++ renderer/hooks/useRtlInput.js
@@ -3,15 +3,15 @@ import { useEffect, useState } from 'react'
 import { calculateRtlDirection } from '@texttree/notepad-rcl'
 
 export const useRtlInput = (initialValue = '') => {
-  const [value, setValue] = useState(initialValue)
+  const [value, setValue] = useState(initialValue || '')
   const [direction, setDirection] = useState('ltr')
 
   useEffect(() => {
-    setDirection(calculateRtlDirection(value))
+    setDirection(calculateRtlDirection(value || ''))
   }, [value])
 
   const handleChange = (event) => {
-    const newValue = event.target.value
+    const newValue = event.target?.value || ''
     setValue(newValue)
     setDirection(calculateRtlDirection(newValue))
   }
