cddaeb9b5423ad3d4a00935c8e7bc00c9d4f2a2c
DenisArger <den.arger@gmail.com>
2025-03-04 18:59:06 +0300

    feat: add typeProject for useGetTnResource

--- renderer/hooks/useGetTnResource.js
+++ renderer/hooks/useGetTnResource.js
@@ -1,20 +1,29 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
-export function useGetTnResource({ id, resource, mainResource, chapter, wholeChapter }) {
-  const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState([]);
+export function useGetTnResource({
+  id,
+  resource,
+  mainResource,
+  chapter,
+  wholeChapter,
+  typeProject,
+}) {
+  const [isLoading, setIsLoading] = useState(true)
+  const [data, setData] = useState([])
   useEffect(() => {
-    const tn = window.electronAPI.getTN(id, resource, mainResource, chapter);
+    const tn = window.electronAPI.getTN(id, resource, mainResource, chapter)
     if (wholeChapter === false) {
-      const verses = window.electronAPI.getChapter(id, chapter);
+      const verses = window.electronAPI.getChapter(id, typeProject, chapter)
       const versesEnabled = Object.keys(verses).reduce((acc, key) => {
-        acc[key] = verses[key].enabled;
-        return acc;
-      }, {});
-      setData(tn.filter((v) => versesEnabled[v.verse]));
-    } else { setData(tn) }
-    setIsLoading(false);
-  }, [id, resource, chapter]);
+        acc[key] = verses[key].enabled
+        return acc
+      }, {})
+      setData(tn.filter((v) => versesEnabled[v.verse]))
+    } else {
+      setData(tn)
+    }
+    setIsLoading(false)
+  }, [id, resource, chapter])
 
-  return { isLoading, data };
+  return { isLoading, data }
 }
