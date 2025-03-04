6f1ea2643f44daf1de339955a1564342d156cf4a
DenisArger <den.arger@gmail.com>
2025-03-04 17:35:36 +0300

    feat: add typeProject in useGetTwlResource

--- renderer/hooks/useGetTwlResource.js
+++ renderer/hooks/useGetTwlResource.js
@@ -1,20 +1,29 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
-export function useGetTwlResource({ id, resource, mainResource, chapter, wholeChapter }) {
-  const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState([]);
+export function useGetTwlResource({
+  id,
+  typeProject,
+  resource,
+  mainResource,
+  chapter,
+  wholeChapter,
+}) {
+  const [isLoading, setIsLoading] = useState(true)
+  const [data, setData] = useState([])
   useEffect(() => {
-    const twl = window.electronAPI.getTWL(id, resource, mainResource, chapter);
+    const twl = window.electronAPI.getTWL(id, resource, mainResource, chapter)
     if (wholeChapter === false) {
-      const verses = window.electronAPI.getChapter(id, chapter);
+      const verses = window.electronAPI.getChapter(id, typeProject, chapter)
       const versesEnabled = Object.keys(verses).reduce((acc, key) => {
-        acc[key] = verses[key].enabled;
-        return acc;
-      }, {});
-      setData(twl.filter((v) => versesEnabled[v.verse]));
-    } else { setData(twl) }
-    setIsLoading(false);
-  }, [id, resource, chapter]);
+        acc[key] = verses[key].enabled
+        return acc
+      }, {})
+      setData(twl.filter((v) => versesEnabled[v.verse]))
+    } else {
+      setData(twl)
+    }
+    setIsLoading(false)
+  }, [id, resource, chapter])
 
-  return { isLoading, data };
+  return { isLoading, data }
 }
