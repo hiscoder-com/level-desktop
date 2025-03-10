cc68a10700a842e374fb8f505b7f6884f1ae1666
DenisArger <den.arger@gmail.com>
2025-03-10 10:52:09 +0300

    fix: optimize code

--- renderer/hooks/useGetTnResource.js
+++ renderer/hooks/useGetTnResource.js
@@ -15,37 +15,38 @@ export function useGetTnResource({
     let tn
 
     if (typeProject === 'obs') {
-      tn = window.electronAPI.getTNObs(id, resource, mainResource, chapter)
-
-      tn = tn.flatMap((notesArray, index) => {
-        return notesArray.map((note) => ({
-          verse: [(index + 1).toString()],
-          ID: note.id,
-          Note: note.text,
-          Quote: note.title,
-        }))
-      })
-
-      if (wholeChapter === false) {
-        const verses = window.electronAPI.getChapter(id, chapter, typeProject)
-        const versesEnabled = Object.keys(verses).reduce((acc, key) => {
-          acc[key] = verses[key].enabled
-          return acc
-        }, {})
-
-        tn = tn.filter((v) => versesEnabled[v.verse])
-      }
+      tn = window.electronAPI
+        .getTNObs(id, resource, mainResource, chapter)
+        .flatMap((notesArray, index) =>
+          Array.isArray(notesArray)
+            ? notesArray.map((note) => ({
+                verse: (index + 1).toString(),
+                ID: note.id,
+                Note: note.text,
+                Quote: note.title,
+              }))
+            : [
+                {
+                  verse: (index + 1).toString(),
+                  ID: notesArray.id,
+                  Note: notesArray.text,
+                  Quote: notesArray.title,
+                },
+              ]
+        )
     } else {
       tn = window.electronAPI.getTN(id, resource, mainResource, chapter)
-      if (wholeChapter === false) {
-        const verses = window.electronAPI.getChapter(id, chapter, typeProject)
-        const versesEnabled = Object.keys(verses).reduce((acc, key) => {
-          acc[key] = verses[key].enabled
-          return acc
-        }, {})
-        tn = tn.filter((v) => versesEnabled[v.verse])
-      }
     }
+
+    if (!wholeChapter) {
+      const verses = window.electronAPI.getChapter(id, chapter, typeProject)
+      const versesEnabled = Object.keys(verses).reduce((acc, key) => {
+        acc[key] = verses[key].enabled
+        return acc
+      }, {})
+      tn = tn.filter((item) => versesEnabled[item.verse])
+    }
+
     setData(tn)
     setIsLoading(false)
   }, [id, resource, mainResource, chapter, wholeChapter, typeProject])
